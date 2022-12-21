ec526815f00146d405dd0e5b3a6aaafdc8f73a93
Sivakumar K <sivakumar.k@acheron-tech.com>
2022-12-21 10:23:52 +0530

    first Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddUpdateComponent } from './components/add-update/add-update.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { EmployeeViewComponent } from './components/employee-view/employee-view.component';
+import { EmployeeDetailsViewComponent } from './components/employee-details-view/employee-details-view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddUpdateComponent,
+    NavBarComponent,
+    EmployeeViewComponent,
+    EmployeeDetailsViewComponent
   ],
   imports: [
     BrowserModule,
